0985f183a6ae21e0a248ad48a1c37fc3b907d2da
Sawka01 <8744934@gmail.com>
2021-02-10 07:20:21 +0500

    Initial Commit

---
+++ TravelDiscoveryApp/TravelDiscoveryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelDiscoveryAppApp.swift
+//  TravelDiscoveryApp
+//
+//  Created by Macbook on 10.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct TravelDiscoveryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
