0c86467836253d24edffa163324687bd50570fde
Sawka01 <8744934@gmail.com>
2021-02-11 03:15:44 +0500

    Lesson 8

--- TravelDiscoveryApp/Discover/DiscoverCategoriesView.swift
+++ TravelDiscoveryApp/Discover/DiscoverCategoriesView.swift
@@ -21,29 +21,65 @@ struct DiscoverCategoriesView: View {
         ScrollView(.horizontal, showsIndicators: false) {
             HStack(alignment: .top, spacing: 14) {
                 ForEach(categories, id: \.self) { category in
-                    VStack(spacing: 8) {
-                        Image(systemName: category.imageName)
-                            .font(.system(size: 20))
-                            .foregroundColor(Color(#colorLiteral(red: 1, green: 0.6154896165, blue: 0.1726504923, alpha: 1)))
-                            .frame(width: 64, height: 64)
-                            .background(Color.white)
-                            .cornerRadius(64)
-                        Text(category.name)
-                            .font(.system(size: 12, weight: .semibold))
-                            .multilineTextAlignment(.center)
-                            .foregroundColor(.white)
-                    }.frame(width: 68)
+                    NavigationLink(
+                        destination: CategoryDetailsView(),
+                        label: {
+                            VStack(spacing: 8) {
+                                Image(systemName: category.imageName)
+                                    .font(.system(size: 20))
+                                    .foregroundColor(Color(#colorLiteral(red: 1, green: 0.6154896165, blue: 0.1726504923, alpha: 1)))
+                                    .frame(width: 64, height: 64)
+                                    .background(Color.white)
+                                    .cornerRadius(64)
+                                Text(category.name)
+                                    .font(.system(size: 12, weight: .semibold))
+                                    .multilineTextAlignment(.center)
+                                    .foregroundColor(.white)
+                            }.frame(width: 68)
+                        })
                 }
             }.padding(.horizontal)
         }
     }
 }
 
+struct CategoryDetailsView: View {
+    var body: some View {
+        ScrollView {
+            ForEach(0..<5, id: \.self) { num in
+                VStack(alignment: .leading, spacing: 0) {
+                    Image("art1")
+                        .resizable()
+                        .scaledToFill()
+                    Text("Demo")
+                        .font(.system(size: 12, weight: .semibold))
+                        .padding()
+                }.asTile()
+                .padding()
+            }
+            
+        }.navigationBarTitle("Category", displayMode: .inline)
+    }
+}
+
 struct DiscoverCategoriesView_Previews: PreviewProvider {
     static var previews: some View {
-        ZStack {
-            Color.orange
-            DiscoverCategoriesView()
+        NavigationView {
+            CategoryDetailsView()
         }
+
+        DiscoverView()
+        //        ZStack {
+        //            Color.orange
+        //            DiscoverCategoriesView()
+        //        }
+
+        //        NavigationView {
+        //            NavigationLink(
+        //                destination: Text("Transition Screen"),
+        //                label: {
+        //                    Text("Link")
+        //                })
+        //        }
     }
 }
