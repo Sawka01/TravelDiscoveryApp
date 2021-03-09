bb1611c945fefe4e7fbf7de203532cbec11c459b
Sawka01 <8744934@gmail.com>
2021-03-09 07:32:06 +0500

    Lesson 19

--- TravelDiscoveryApp/Discover/DestinationGroup/DestinationHeaderContainer.swift
+++ TravelDiscoveryApp/Discover/DestinationGroup/DestinationHeaderContainer.swift
@@ -9,9 +9,8 @@ import SwiftUI
 
 struct DestinationHeaderContainer: UIViewControllerRepresentable {
     func makeUIViewController(context: Context) -> UIViewController {
-        let redVC = UIViewController()
-        redVC.view.backgroundColor = .red
-        return redVC
+        let pvc = CustomPageViewController()
+        return pvc
     }
 
     typealias UIViewControllerType = UIViewController
@@ -21,8 +20,66 @@ struct DestinationHeaderContainer: UIViewControllerRepresentable {
     }
 }
 
+class CustomPageViewController: UIPageViewController, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
+
+    func presentationCount(for pageViewController: UIPageViewController) -> Int {
+        return allControllers.count
+    }
+
+    func presentationIndex(for pageViewController: UIPageViewController) -> Int {
+        return 0
+    }
+
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let index = allControllers.firstIndex(of: viewController) else { return nil}
+
+        if index == 0 { return nil }
+        return allControllers[index - 1]
+    }
+
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let index = allControllers.firstIndex(of: viewController) else { return nil}
+
+        if index == allControllers.count - 1 { return nil }
+        return allControllers[index + 1]
+    }
+
+
+    let firstVC = UIHostingController(rootView: Text("First View Controller"))
+    let secondVC = UIHostingController(rootView: Text("Second"))
+    let thirdVC = UIHostingController(rootView: Text("Third"))
+
+    lazy var allControllers: [UIViewController] = [
+        firstVC, secondVC, thirdVC
+    ]
+
+    init() {
+        UIPageControl.appearance().pageIndicatorTintColor = UIColor.systemGray5
+        UIPageControl.appearance().currentPageIndicatorTintColor = .red
+
+        super.init(transitionStyle: .scroll,
+                   navigationOrientation: .horizontal,
+                   options: nil)
+
+        setViewControllers([firstVC],
+                           direction: .forward,
+                           animated: true,
+                           completion: nil)
+
+        self.dataSource = self
+        self.delegate = self
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
 struct DestinationHeaderContainer_Previews: PreviewProvider {
     static var previews: some View {
         DestinationHeaderContainer()
+        NavigationView {
+            PopularDestinationDetailsView(destination: .init(name: "Paris", country: "France", imageName: "eiffel_tower", latitude: 48.859565, longitude: 2.353235))
+        }
     }
 }
