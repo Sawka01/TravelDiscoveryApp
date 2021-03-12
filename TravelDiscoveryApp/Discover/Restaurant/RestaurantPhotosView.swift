1d3427357edb9ce7a53de1b620771a0821963c77
Sawka01 <8744934@gmail.com>
2021-03-13 01:17:02 +0500

    Lesson 27

--- TravelDiscoveryApp/Discover/Restaurant/RestaurantPhotosView.swift
+++ TravelDiscoveryApp/Discover/Restaurant/RestaurantPhotosView.swift
@@ -6,27 +6,36 @@
 //
 
 import SwiftUI
+import Kingfisher
 
 struct RestaurantPhotosView: View {
+
+    let photoUrlStrings = [
+        ""
+    ]
+
     var body: some View {
-        ScrollView {
+        GeometryReader { proxy in
+            ScrollView {
 
-            LazyVGrid(columns: [
-                GridItem(.fixed(125), spacing: 4),
-                GridItem(.fixed(125), spacing: 4),
-                GridItem(.fixed(125))
+                LazyVGrid(columns: [
+                    GridItem(.adaptive(minimum: proxy.size.width / 3 - 4, maximum: 300), spacing: 2)
 
-            ], spacing: 4, content: {
+                ], spacing: 4, content: {
 
-                ForEach(0..<15, id: \.self) { num in
-                    Text("Placeholder")
-                        .padding()
-                        .background(Color.red)
-                }
+                    ForEach(photoUrlStrings, id: \.self) { urlString in
+                        KFImage(URL(string: urlString))
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: proxy.size.width / 3 - 3, height: proxy.size.width / 3 - 3)
+                            .clipped()
+                    }
 
-            })
+                }).padding(.horizontal, 2)
 
-        }.navigationBarTitle("All Photos", displayMode: .inline)
+            }
+        }
+        .navigationBarTitle("All Photos", displayMode: .inline)
     }
 }
 
